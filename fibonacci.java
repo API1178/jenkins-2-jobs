795671b76b78c20c357d8f4cf1643d8e0309f2a5
Pranav Bakre <pranav.bakre@apisero.com>
2021-06-13 14:08:17 +0530

    take n from args itself and  not from Input Stream

--- fibonacci.java
+++ fibonacci.java
@@ -1,9 +1,9 @@
 public class fibonacci {
     public static void main(String [] args) {
         int n;
-        var console = System.console();
-        System.out.println("Enter the length");
-        n=Integer.parseInt(console.readLine());
+        // var console = System.console();
+        // System.out.println("Enter the length");
+        n=Integer.parseInt(args[0]);
         int a=-1,b=1;
         System.out.println("Fibonacci Series till " + n);;
         for(int i=0;i<n;i++) {
